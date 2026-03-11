21669c835e569020c2542c1ef082c1325bfb8564
20230036 <20230036@students.htl-perg.ac.at>
2026-03-11 14:17:24 +0100

    asdf

--- Pflanzenbewässerungssystem/Plant/src/main.cpp
+++ Pflanzenbewässerungssystem/Plant/src/main.cpp
@@ -5,8 +5,10 @@
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
 #include <SPIFFS.h>
+#include <LittleFS.h>
 #include <fstream>
 #include <iostream>
+
 
 #define PUMP1 21 //da preprocessor geht afoch drüba und schreibt überall wo PUMP1 steht de zoi 21 hi. (ziemlich warscheinlich)
 #define PUMP2 20
@@ -14,13 +16,13 @@
 
 // für die schule
 const char *ssid = "HTL-Perg Gast";
-const char *password = "FIT2024!";
+const char *password = "FIT2025!";
 
 // für zuhause
 // const char *ssid = "Friedhof";
 // const char *password = "EW749DF109";
 
-AsyncWebServer webServer(80); //80 is da port
+AsyncWebServer server(80); //80 is da port
 String header;
 String output26State = "off";
 String output27State = "off";
@@ -91,52 +93,7 @@ void flushAll(int seconds) {
     //...
 }
 
-// int manipulateSecPump(int pumpNum, boolean increase, int intervallInMillSecs[]) {
-//     pumpNum --;
-//     if (increase) {
-//         if (intervallInMillSecs[pumpNum] < 3600000) {
-//             intervallInMillSecs[pumpNum] += 100;
-//             preferences.putInt("duration1", intervallInMillSecs[pumpNum]);
-//             Serial.println("flow duration: " + intervallInMillSecs[pumpNum]);
-
-//         } else {
-//             Serial.println("Maximum von 1200 sekunden erreicht (20min)");
-//         }
-//     } else {
-//         if (intervallInMillSecs[pumpNum] > 100) {
-//             intervallInMillSecs[pumpNum] -= 100;
-//             preferences.putInt("duration1", intervallInMillSecs[pumpNum]);
-//             Serial.println("flow duration: " + intervallInMillSecs[pumpNum]);
-            
-//         } else {
-//             Serial.println("Minimum von 0.1 sekunden erreicht (100 millsec.)");
-//         }
-//     }
-//     return intervallInMillSecs[pumpNum];
-// }
-
-// int manipulateDayPump(int pumpNum, boolean increase, int daysGoal[]) {
-//     pumpNum--;
-//     Serial.println(increase);
-//     if (increase) {
-//         if (daysGoal[pumpNum] < 365) {
-//             daysGoal[pumpNum]++;
-//             preferences.putInt("dayGoal3", daysGoal[pumpNum]);
-//         } else {
-//             Serial.println("Maximum von 1 Jahr (365 Tage) erreicht");
-//         }
-//     } else {
-//         if (daysGoal[pumpNum] > 1) {
-//             daysGoal[pumpNum]--;
-//             preferences.putInt("dayGoal1", daysGoal[pumpNum]);
-//         } else {
-//             Serial.println("Minimum von 1 Tag erreicht");
-//         }
-//     }
-//     return daysGoal[pumpNum];
-// }
-
-void webserverOnUse() {
+/*void webserverOnUse() {
     webServer.on("/usePump1", HTTP_GET, [](AsyncWebServerRequest *request) {
         usePump(intervalInMillSec[0], 1);
         timeStamp[0] = timeClient.getEpochTime();
@@ -160,124 +117,7 @@ void webserverOnUse() {
         Serial.println("pump3 was used");
         request->send(200);
     });
-}
-
-// void webserverOnNoCounter() {
-//     webServer.on("/usePump1IgnoreCTR", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         usePump(intervalInMillSec[0], 1);
-//         Serial.println("pump1 was used");
-//         request->send(200); 
-//     });
-
-//     webServer.on("/usePump2IgnoreCTR", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         usePump(intervalInMillSec[1], 2);
-//         Serial.println("pump2 was used");
-//         request->send(200); 
-//     });
-
-//     webServer.on("/usePump3IgnoreCTR", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         usePump(intervalInMillSec[2], 3);
-//         Serial.println("pump3 was used");
-//         request->send(200); 
-//     });
-// }
-
-// void webserverProcessStatus() {
-//     webServer.on("/startProcess", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         running = true;
-//         preferences.putBool("running", running);
-//         Serial.println("process started"); 
-//         request->send(200, "text/plain", "true");
-//     });
-
-//     webServer.on("/stopProcess", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         running = false;
-//         preferences.putBool("running", running);
-//         Serial.println("progress stopped"); 
-//         request->send(200, "text/plain", "false");
-//     });
-// }
-
-// void webserverOnIncreaseSec() {
-//     webServer.on("/increaseSecPump1", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds1", intervalInMillSec[0]);
-//         Serial.println("duration increased 1"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[0] = manipulateSecPump(1, true, intervalInMillSec)));
-//     });
-
-//     webServer.on("/increaseSecPump2", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds2", intervalInMillSec[1]);
-//         Serial.println("duration increased 2"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[1] = manipulateSecPump(2, true, intervalInMillSec)));
-//     });
-
-//     webServer.on("/increaseSecPump3", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds3", intervalInMillSec[2]);
-//         Serial.println("duration increased 3"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[2] = manipulateSecPump(3, true, intervalInMillSec)));
-//     });
-// }
-
-// void webserverOnDecreaseSec() {
-//     webServer.on("/decreaseSecPump3", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds3", intervalInMillSec[2]);
-//         Serial.println("duration decreased 3"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[2] = manipulateSecPump(3, false, intervalInMillSec)));
-//     });
-
-//     webServer.on("/decreaseSecPump2", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds2", intervalInMillSec[1]);
-//         Serial.println("duration decreased 2"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[1] = manipulateSecPump(2, false, intervalInMillSec)));
-//     });
-
-//     webServer.on("/decreaseSecPump1", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Seconds1", intervalInMillSec[0]);
-//         Serial.println("duration decreased 1"); 
-//         request->send(200, "text/plain", String(intervalInMillSec[0] = manipulateSecPump(1, false, intervalInMillSec)));
-//     });
-// }
-
-// void webserverOnIncreaseDays() {
-//     webServer.on("/increaseDaysPump3", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal3", dayGoal[2]);
-//         Serial.println("days increased 3"); 
-//         request->send(200, "text/plain", String(dayGoal[2] = manipulateDayPump(3, true, dayGoal)));
-//         //request->send(200, "text/plain", "Hello");
-//     });
-
-//     webServer.on("/increaseDaysPump2", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal2", dayGoal[1]);
-//         Serial.println("days increased 2"); 
-//         request->send(200, "text/plain", String(dayGoal[1] = manipulateDayPump(2, true, dayGoal)));
-//     });
-
-//     webServer.on("/increaseDaysPump1", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal1", dayGoal[0]);
-//         Serial.println("days increased 1"); 
-//         request->send(200, "text/plain", String(dayGoal[0] = manipulateDayPump(1, true, dayGoal)));
-//     });
-// }
-
-// void webserverOnDecreaseDays() {
-//     webServer.on("/decreaseDaysPump1", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal1", dayGoal[0]);
-//         Serial.println("days decreased 1"); 
-//         request->send(200, "text/plain", String(dayGoal[0] = manipulateDayPump(1, false, dayGoal)));
-//     });
-
-//     webServer.on("/decreaseDaysPump2", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal2", dayGoal[1]);
-//         Serial.println("days decreased 2"); 
-//         request->send(200, "text/plain", String(dayGoal[1] = manipulateDayPump(2, false, dayGoal)));
-//     });
-
-//     webServer.on("/decreaseDaysPump3", HTTP_GET, [](AsyncWebServerRequest *request) {
-//         preferences.putInt("Daygoal3", dayGoal[2]);
-//         Serial.println("days decreased 3"); 
-//         request->send(200, "text/plain", String(dayGoal[2] = manipulateDayPump(3, false, dayGoal)));
-//     });
-// }
+}*/
 
 /** Setup (macht der 1 mal beim Einschalten) */
 void setup() {
@@ -289,31 +129,42 @@ void setup() {
     pinMode(PUMP2, OUTPUT);
     pinMode(PUMP3, OUTPUT);
 
-    Serial.println("Initializing SPIFFS...");
+    /*Serial.println("Initializing SPIFFS...");
     if (!SPIFFS.begin(true)) {
         Serial.println("An Error has occurred while mounting SPIFFS");
         return;
-    }
-    webServer.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+    }*/
+    //webServer.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
     // Connect to Wi-Fi network with SSID and password
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
+    Serial.print("Verbinde mit WLAN");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
 
     Serial.println();
-    webServer.begin();
+    // webServer.begin();
     timeClient.begin();
 
     // Print local IP address and start web server
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
+    Serial.println("\nVerbunden! IP: ");
     Serial.println(WiFi.localIP());
 
+
+    if (!LittleFS.begin()) {
+        Serial.println("Fehler beim Start von LittleFS");
+        return;
+    }
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+
+    server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "application/json", "{\"status\": \"ok\", \"uptime\": " + String(millis()) + "}");
+    });
+    server.begin();
 }
 
 void loop() {
